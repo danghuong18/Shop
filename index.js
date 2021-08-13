fff4e05fef2a6500389db92b02df2f120fe863c4
James Lee <danghuong18@gmail.com>
2021-08-13 14:47:17 +0700

    Add some new files

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ const ProductCodeModel = require('./model/productCodeModel');
 const cookieParser = require('cookie-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "./")));
+
+app.get('/', (req, res)=>{
+    res.render('pages/index');
+});
 
 app.listen(process.env.PORT || 3000);
