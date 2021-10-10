bb3c329402876be48e3f4cf22239c1a69d90fe3a
thaidd7b <thaidd7b@gmail.com>
2021-10-10 09:24:54 +0700

    cart query

--- model/cartModel.js
+++ model/cartModel.js
@@ -19,3 +19,27 @@ const CartSchema = mongoose.Schema(
 const CartModel = mongoose.model("cart", CartSchema);
 
 module.exports = CartModel;
+
+async function test() {
+  try {
+    await CartModel.updateOne(
+      { _id: "61592f0b7e919eb913617cc6" },
+      { $set: { "listProduct.$[i].selected": 1 } },
+      {
+        arrayFilters: [
+          {
+            "i.productID": {
+              $in: ["614c4755b8aa0667a8c7cc2b", "614ca40072b39f49e4338367"],
+            },
+          },
+        ],
+      }
+    );
+    let data = await CartModel.findOne({ _id: "61592f0b7e919eb913617cc6" });
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+test();
